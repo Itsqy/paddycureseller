ddcafdc73affe7abe3bc1ef6066a43d220fcfc4b
muhammad rifqi syatria <syatriarifqi@gmail.com>
2023-06-05 20:32:41 +0700

    fixing ui register

--- app/src/main/java/com/darkcoder/paddycureseller/ui/customview/MyEditTextPassword.kt
+++ app/src/main/java/com/darkcoder/paddycureseller/ui/customview/MyEditTextPassword.kt
@@ -1,4 +1,4 @@
-package com.darkcoder.paddycure.ui.customview
+package com.darkcoder.paddycureseller.ui.customview
 
 import android.content.Context
 import android.graphics.Canvas
@@ -11,8 +11,7 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.ContextCompat
-
-import com.darkcoder.paddycure.R
+import com.darkcoder.paddycureseller.R
 import com.google.android.material.textfield.TextInputEditText
 
 class MyEditTextPassword : TextInputEditText, View.OnTouchListener {
